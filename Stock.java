2e9f29ccca10d52c4af28b76277fca5aff35be85
Paul DELERUE <paul.delerue@student.junia.com>
2023-11-28 15:51:37 +0100

    MAJ de stock

--- Stock.java
+++ Stock.java
@@ -10,16 +10,28 @@ public class Stock {
 		return(this.liste.get(produit));
 	}
 	
+	// setters
+	
+	void addToStock(String produit,int nbr) {
+		this.liste.put(produit, nbr);
+	}
+	
 	// gestion des stocks
 	
-	void diminuerStock(String produit, int nbr) {
+	void diminuerStock(String produit, int nbr) {// permet de diminuer les quantites
 		if(this.liste.get(produit)>=nbr) {
 			this.liste.replace(produit, this.liste.get(produit)-nbr);
 		}
 	}
 	
-	void ajouterStock(String produit, int nbr) {
+	void ajouterStock(String produit, int nbr) { // permet d'augmenter les quantites
 		this.liste.replace(produit, this.liste.get(produit)+nbr);
 	}
+	
+	void copyStock(Stock original) {// permet de copier la liste d'un autre stock
+		this.liste=original.liste;
+	}
+	
+	
 
 }
